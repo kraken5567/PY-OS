e187ed9bb8c719f8d16e7b6a510499774cec3e63
kraken5567 <paddymurph2005@gmail.com>
2023-03-25 13:41:49 -0400

    Browser implemented (simple)

---
+++ SystemPrograms/Browser/Browser.py
@@ -0,0 +1,25 @@
+def Main(OS):
+    import sys
+    from PyQt5.QtCore import QUrl
+    from PyQt5.QtWidgets import QApplication, QVBoxLayout, QWidget
+    from PyQt5.QtWebEngineWidgets import QWebEngineView
+
+    class Browser(QWidget):
+        def __init__(self):
+            super().__init__()
+            self.setWindowTitle('Py Chart')
+            self.setGeometry(100, 100, 800, 600)
+            self.create_browser()
+
+        def create_browser(self):
+            self.browser = QWebEngineView()
+            self.browser.setUrl(QUrl('https://www.google.com'))
+            layout = QVBoxLayout()
+            layout.addWidget(self.browser)
+            self.setLayout(layout)
+
+    if True:
+        app = QApplication(sys.argv)
+        browser = Browser()
+        browser.show()
+        browser.exit(app.exec_())
