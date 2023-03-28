2b3a674608e14474f12304fa7c70a0fd6d3b50d7
kraken5567 <paddymurph2005@gmail.com>
2023-03-28 16:22:18 -0400

    commented out browser (which is broken)

--- SystemPrograms/Browser/Browser.py
+++ SystemPrograms/Browser/Browser.py
@@ -1,25 +1,28 @@
 def Main(OS):
-    import sys
-    from PyQt5.QtCore import QUrl
-    from PyQt5.QtWidgets import QApplication, QVBoxLayout, QWidget
-    from PyQt5.QtWebEngineWidgets import QWebEngineView
 
-    class Browser(QWidget):
-        def __init__(self):
-            super().__init__()
-            self.setWindowTitle('Py Chart')
-            self.setGeometry(100, 100, 800, 600)
-            self.create_browser()
+    print("DOES NOT WORK PROPERLY!")
 
-        def create_browser(self):
-            self.browser = QWebEngineView()
-            self.browser.setUrl(QUrl('https://www.google.com'))
-            layout = QVBoxLayout()
-            layout.addWidget(self.browser)
-            self.setLayout(layout)
+    # import sys
+    # from PyQt5.QtCore import QUrl
+    # from PyQt5.QtWidgets import QApplication, QVBoxLayout, QWidget
+    # from PyQt5.QtWebEngineWidgets import QWebEngineView
 
-    if True:
-        app = QApplication(sys.argv)
-        browser = Browser()
-        browser.show()
-        browser.exit(app.exec_())
+    # class Browser(QWidget):
+    #     def __init__(self):
+    #         super().__init__()
+    #         self.setWindowTitle('Py Chart')
+    #         self.setGeometry(100, 100, 800, 600)
+    #         self.create_browser()
+
+    #     def create_browser(self):
+    #         self.browser = QWebEngineView()
+    #         self.browser.setUrl(QUrl('https://www.google.com'))
+    #         layout = QVBoxLayout()
+    #         layout.addWidget(self.browser)
+    #         self.setLayout(layout)
+
+    # if True:
+    #     app = QApplication(sys.argv)
+    #     browser = Browser()
+    #     browser.show()
+    #     browser.exit(app.exec_())
