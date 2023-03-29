6dd76fc0f5ed5ec85d76e4302ace70333ceb1c9d
kraken5567 <paddymurph2005@gmail.com>
2023-03-29 19:00:24 -0400

    +creation(file&fldr) & txt based editing

--- SystemPrograms/FileFinder/FileFinder.py
+++ SystemPrograms/FileFinder/FileFinder.py
@@ -5,65 +5,99 @@ def Main(OS):
 
     selection = T.StringVar()
 
-    def ReadFolderContents(Location):
+    FileManager = T.Toplevel(bg="dark gray")
+    FileManager.title("File Manager")
+
+    # Evil Global
+    global Location
+    Location = os.getcwd()
+    #--------------------
+
+    lister = T.Label(FileManager)
+    lister.config(text=Location,bg="dark gray")
+    lister.grid(row=0,columnspan=3,sticky='we')
+
+    Error = T.Label(FileManager,fg="red",bg="black")
+    Error.grid(row=1000,columnspan=3,sticky='we')
+
+    # functions
+    def ReadFolderContents():
+        global Location
+        lister.config(text=Location)
         fileFolderList = os.listdir(Location)
         return fileFolderList
-    
-    FileManager = T.Toplevel()
-    FileManager.title("File Manager")
-    Location = os.getcwd()
 
-    lister = T.Label(FileManager,text=Location)
-    T.Scrollbar(lister)
+    def updateContents():
+        contents = ReadFolderContents()
+        allRButtons = []
+        for x, y in enumerate(contents):
+            if ("." in y[-5:]) and not ("." in y[:1]):
+                a = T.Radiobutton(FileManager, text = y, variable = selection, indicatoron=0, value = y, bg="blue",fg="Red")
+            else:
+                a = T.Radiobutton(FileManager, text = y, variable = selection, indicatoron=0, value = y,bg="yellow",fg="red")
+            a.grid(row=x+1,column=0,sticky='we')
+            allRButtons.append(a)
+            
+        return allRButtons
 
-    contents = ReadFolderContents(Location)
 
-    allRButtons = []
-    for x, y in enumerate(contents):
-        if ("." in y[-5:]) and not ("." in y[:1]):
-            a = T.Radiobutton(FileManager, text = y, variable = selection, value = y, bg="blue")
-        else:
-            a = T.Radiobutton(FileManager, text = y, variable = selection, value = y,bg="yellow")
-        a.grid(row=x+1,column=0,sticky='we')
-        allRButtons.append(a)
+    global allRButtons
+    allRButtons = updateContents()
 
-    lister.grid(row=0,column=1)
-    #button functions
 
-    #File stuff
-    def OpenFile(select,Location):
+    # file stuff
+    def OpenFile(select):
+        global Location
         if select.endswith((".png","jpg","jpeg",".gif",".bmp",".tif",".tiff")):
-            print("Not Yet Implemented, Soon though")
+            Error.config(text="Image editting not added yet!")
         elif not select.endswith((".png","jpg","jpeg",".gif",".bmp",".tif",".tiff")):
             import SystemPrograms.Noterer.Noterer as Nt
             importlib.reload(Nt)
             Nt.ReadEdit(select,Location)
             del Nt
-    #def NewFile(select,Location):
-    #def EditFile(select,Location):
+    
+    def NewFile(select):
+        global Location
+        if not os.path.exists(os.path.join(Location, NewItem.get())):
+            with open(os.path.join(Location, NewItem.get()), 'w') as NewFile:
+                NewFile.write("")
+                NewFile.close()
 
     #Folder Stuff
-    #def OpenFolder(select,Location):
-    #def NewFolder(select,Location):
+    def OpenFolder(select):
+        global Location
+        Location = os.path.join(Location, select)
+        global allRButtons
+        for x in allRButtons:
+            x.destroy()
+        allRButtons = updateContents()
+        return Location
 
+    def NewFolder(select):
+        global Location
+        if not os.path.exists(os.path.join(Location, NewItem.get())):
+            os.mkdir(os.path.join(Location, NewItem.get()))
 
-
-    #buttons
-    buttonList = ["OpenFile"]#,"NewFile","EditFile","OpenFolder","NewFolder"]
-    funcList = [OpenFile]#,NewFile,EditFile,OpenFolder,NewFolder]
+    # button stuff
+    buttonList = ["OpenFile","NewFile","OpenFolder","NewFolder"]
+    funcList = [OpenFile,NewFile,OpenFolder,NewFolder]
     y = 0
     z = 0
     n = 0
     for x in buttonList:
-        B = T.Button(FileManager, text=str(x), command=lambda f=funcList[n]: f(selection.get(), Location))
-        B.grid(row=z+8, column=y+1, sticky='we')
+        B = T.Button(FileManager, text=str(x), command=lambda f=funcList[n]: f(selection.get()))
+        B.grid(row=z+1, column=y+1, sticky='we')
         y += 1
-        if y > 6:
+        if y >= 2:
             y = 0
             z += 1
         n += 1
 
-    
+    EntryLabel = T.Label(FileManager,text="Entry for 'New' Items")
+    EntryLabel.grid(row=z+1,column=1,columnspan=2)
+
+    NewItem= T.Entry(FileManager)
+    NewItem.grid(row=z+2,column=1,columnspan=2)
 
 def FFImported():
     from tkinter import filedialog as Fd
