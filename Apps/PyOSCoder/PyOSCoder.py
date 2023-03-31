8596bb96a1017983fa8f905963dcb4ea3033ca8b
kraken5567 <paddymurph2005@gmail.com>
2023-03-30 20:56:54 -0400

    Fully implemented Coder with custom FileManager

--- Apps/PyOSCoder/PyOSCoder.py
+++ Apps/PyOSCoder/PyOSCoder.py
@@ -1,33 +1,35 @@
 def Main(OS):
-    import tkinter as T
-    from tkinter import filedialog as fd
     import SystemPrograms.FileFinder.FileFinder as FF
+    import importlib.util
+    importlib.reload(FF)
+    
+    FF.FFImported(OS)
+    
+def coder(file_text):
+    import tkinter as T
+
     Coder = T.Toplevel()
     Coder.title("PyOS Coder")
 
-    file_text = FF.FFImported()
-
     code_vis = T.Text(Coder,height=20,font=8)
     T.Scrollbar(code_vis)
-
-    lines = file_text.split("\n")
+    file = open(file_text, "r")
+    file = file.read()
+    lines = file.split("\n")
     for row, line in enumerate(lines):
         code_vis.insert(f"{row+1}.0", line + "\n")
-    code_vis.grid(row=0, column=0, padx=10, pady=10, sticky='we')
+    code_vis.grid(row=0, columnspan=3, padx=10, pady=10, sticky='we')
     Coder.columnconfigure(0, weight=1)
 
     def Exit():
         Coder.destroy()
     exit = T.Button(Coder,text="Exit",bg="red",command=Exit)
-    exit.grid(row=2, column=1, padx=10, pady=10, sticky='e')
+    exit.grid(row=2, column=2, padx=10, pady=10, sticky='e')
 
     def Apply():
         code = code_vis.get("1.0", "end-1c")
-        code_name = fd.asksaveasfilename(defaultextension=".py",filetypes=[("Python Files", "*.py"),("Json Files", "*.json"),("Text Files", "*.txt"), ("All Files", "*.*")])
-        if code_name:
-            with open(code_name, "w") as file:
-                file.write(code)
+        with open(file_text, "w") as file:
+            file.write(code)
     apply = T.Button(Coder,text="Save",bg="blue",command=Apply)
     apply.grid(row=2, column=0, padx=10, pady=10, sticky='w')
-
     Coder.mainloop()
--- SystemPrograms/FileFinder/FileFinder.py
+++ SystemPrograms/FileFinder/FileFinder.py
@@ -101,16 +101,96 @@ def Main(OS):
     NewItem= T.Entry(FileManager)
     NewItem.grid(row=z+2,column=1,columnspan=2)
 
-def FFImported():
-    from tkinter import filedialog as Fd
+def FFImported(OS):
     import tkinter as T
     import os
+    import importlib.util
 
-    FileManager = T.Toplevel()
-    FileManager.withdraw()
+    selection = T.StringVar()
+    global FileManager
+    FileManager = T.Toplevel(OS)
     FileManager.title("File Manager")
-    Location = os.path.abspath(os.getcwd())
-    FileManager.filename = Fd.askopenfilename(initialdir=Location, title="Select A File", filetypes=(("all files", "*.*"),("App/Sys Icons", "*.png"),("Configs","*.json"),("PyOS Runnables","*.py")))
-    File = open(FileManager.filename,"r")
+    FileManager.transient(OS)
 
-    return File.read()
+    # Evil Global
+    global Location
+    Location = os.getcwd()
+    #--------------------
+
+    lister = T.Label(FileManager)
+    lister.config(text=Location)
+    lister.grid(row=0,columnspan=3,sticky='we')
+
+    Error = T.Label(FileManager,fg="red",bg="black")
+    Error.grid(row=1000,columnspan=3,sticky='we')
+
+    # functions
+    def ReadFolderContents():
+        global Location
+        lister.config(text=Location)
+        fileFolderList = os.listdir(Location)
+        return fileFolderList
+
+    def updateContents():
+        contents = ReadFolderContents()
+        allRButtons = []
+        for x, y in enumerate(contents):
+            if ("." in y[-5:]) and not ("." in y[:1]):
+                a = T.Radiobutton(FileManager, text = y, variable = selection, indicatoron=0, value = y, bg="blue",fg="Red")
+            else:
+                a = T.Radiobutton(FileManager, text = y, variable = selection, indicatoron=0, value = y,bg="yellow",fg="red")
+            a.grid(row=x+1,column=0,sticky='we')
+            allRButtons.append(a)
+            
+        return allRButtons
+
+
+    global allRButtons
+    allRButtons = updateContents()
+
+
+    # file stuff
+    def OpenFile(select):
+        global Location
+        if select.endswith((".png","jpg","jpeg",".gif",".bmp",".tif",".tiff")):
+            pass
+        elif not select.endswith((".png","jpg","jpeg",".gif",".bmp",".tif",".tiff")):
+            import Apps.PyOSCoder.PyOSCoder as Coder
+            importlib.reload(Coder)
+            Coder.coder(os.path.join(Location, select))
+
+            
+
+    #Folder Stuff
+    def OpenFolder(select):
+        global Location
+        Location = os.path.join(Location, select)
+        global allRButtons
+        for x in allRButtons:
+            x.destroy()
+        allRButtons = updateContents()
+        return Location
+
+
+    # button stuff
+    buttonList = ["Open File","Open Folder",]
+    funcList = [OpenFile,OpenFolder,]
+    y = 0
+    z = 0
+    n = 0
+    for x in buttonList:
+        B = T.Button(FileManager, text=str(x), command=lambda f=funcList[n]: f(selection.get()))
+        B.grid(row=z+1, column=y+1, sticky='we')
+        y += 1
+        if y >= 2:
+            y = 0
+            z += 1
+        n += 1
+
+    EntryLabel = T.Label(FileManager,text="Entry for 'New' Items")
+    EntryLabel.grid(row=z+1,column=1,columnspan=2)
+
+    NewItem= T.Entry(FileManager)
+    NewItem.grid(row=z+2,column=1,columnspan=2)
+
+    FileManager.mainloop()
