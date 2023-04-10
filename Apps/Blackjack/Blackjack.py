317a005d4b355d7b07c92185656280a0b005c51d
kraken5567 <paddymurph2005@gmail.com>
2023-04-10 01:18:16 -0400

    displays cards and values + hides dealers 2nd

--- Apps/Blackjack/Blackjack.py
+++ Apps/Blackjack/Blackjack.py
@@ -5,10 +5,6 @@ def Main(OS):
     from PIL import ImageTk, Image
     import random
 
-    table = T.Toplevel(OS)
-    table.title("Blackjack")
-    table.transient(OS)
-    
     #cards ar 2.5in x 3.5in; W x H
     # card images made by Aussiesim from Game-Icons.net
 
@@ -20,11 +16,18 @@ def Main(OS):
     cardsLib = os.listdir(cardDir)
     cards = random.choice(cardsLib)
     card = random.choice(os.listdir(f"{cardDir}\\{cards}"))
-    print(f"{cardDir}\\{cards}\\{card}")
-    table.iconphoto(False, ImageTk.PhotoImage(Image.open(f"{cardDir}\\{cards}\\{card}")))
-
+    
     
     #init values
+    dealerStr = T.StringVar()
+    playerStr = T.StringVar()
+    color = T.StringVar(value="green")
+
+    table = T.Toplevel(OS,bg=color.get())
+    table.title("Blackjack")
+    table.transient(OS)
+    table.iconphoto(False, ImageTk.PhotoImage(Image.open(f"{cardDir}\\{cards}\\{card}")))
+
     cards = os.listdir(cardDir)
 
     #functions
@@ -56,26 +59,95 @@ def Main(OS):
             elif (hand[1] and hand[0]) not in  "AJKQ":
                 a = int(hand[0])
                 b = int(hand[1])
+            elif (hand[1] and hand[0]) in  "AJKQ":
+                if hand[0] in "JKQ":
+                    a = 10
+                elif hand[0] in "A":
+                    a = [1,11]
+                if hand[1] in "JKQ":
+                    b = 10
+                elif hand[1] in "A":
+                    b = [1,11]
             valueDisp.append([a,b])
         return valueDisp
 
-    def display(cardsInPlay,valueDisp,cardDir):
-        print(cardsInPlay,valueDisp)
+    def labelMaker(val,Bool):
+        n = 1
+        if Bool != False:
+            string = "Dealer's Hand: "
+            for x in val:
+                if n < len(val):
+                    string += f"{x} + "
+                else:
+                    string += "?"
+                n += 1
+        elif Bool != True:
+            string = "Player's Hand: "
+            for x in val:
+                if n < len(val):
+                    string += f"{x} + "
+                else:
+                    string += f"{x}"
+                n += 1
+        return string
 
-        # make canvas
+    def cardloader(cardsInHand,Bool,canvas):
+        imgHolder = []
+        cardWidth = 128
+        if Bool == True:
+            height = 0
+            for i, card in enumerate(cardsInHand):
+                imgDir = random.choice(os.listdir(f'{cardDir}\\{card}'))
+                image = Image.open(f"{cardDir}\\{card}\\{imgDir}")
+                image = image.resize((cardWidth, cardWidth))
+                cardImg = ImageTk.PhotoImage(image)
+                imgHolder.append(cardImg)
+                x = i * cardWidth
+                img_tag = canvas.create_image(x, height, image=cardImg, anchor=T.NW)
 
-        for hand in cardsInPlay:
-            for card in hand:
-                # detect first player (dealer) so one card is hidden
-                cardImage = random.choice(os.listdir(f"{cardDir}\\{card}"))
-                # add to list
-                # display on canvas
-                # change position
+                if i + 1 >= len(cardsInHand):
+                    image = Image.open(f"{appDir}\\Blank\\Blank.png")
+                    image = image.resize((cardWidth, cardWidth))
+                    cardImg = ImageTk.PhotoImage(image)
+                    imgHolder.append(cardImg)
+                    x = i * cardWidth
+                    img_tag = canvas.create_image(x, height, image=cardImg, anchor=T.NW)
+
+        elif Bool == False:
+            height = cardWidth
+            for i, card in enumerate(cardsInHand):
+                imgDir = random.choice(os.listdir(f'{cardDir}\\{card}'))
+                image = Image.open(f"{cardDir}\\{card}\\{imgDir}")
+                image = image.resize((cardWidth, cardWidth))
+                cardImg = ImageTk.PhotoImage(image)
+                imgHolder.append(cardImg)
+                x = i * cardWidth
+                img_tag = canvas.create_image(x, height, image=cardImg, anchor=T.SW)
+        else:
+           raise ValueError
+        
+        return imgHolder
+        
+
+    def display(cardsInPlay,valueDisp):
+
+        dealers = T.Canvas(table,bg=color.get(),height=128)
+        dealers.grid(row=1,columnspan=3)
+        dealer_text = labelMaker(valueDisp[0],True)
+        dealer_label = T.Label(table,text = dealer_text,bg=color.get())
+        dealer_label.grid(row=0,columnspan=3)
+        dealer_cards = cardloader(cardsInPlay[0],True,dealers)
+
+        players = T.Canvas(table,bg=color.get(),height=128)
+        players.grid(row=2,columnspan=3)
+        player_text = labelMaker(valueDisp[1],False)
+        player_label = T.Label(table,text = player_text,bg=color.get())
+        player_label.grid(row=3,columnspan=3)
+        player_cards = cardloader(cardsInPlay[1],False,players)
+
+        table.mainloop()
+    
     # game steps
     cardsInPlay = roundStart(cards)
     valueDisp = cardValues(cardsInPlay)
-    display(cardsInPlay,valueDisp,cardDir)
-
-    
-    
-    table.mainloop()
+    display(cardsInPlay,valueDisp)
