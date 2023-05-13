c575d0273e024b971f9d8da0660ba0fe2dca4599
kraken5567 <paddymurph2005@gmail.com>
2023-05-13 15:49:52 -0400

    added a browser

--- SystemPrograms/Browser/Browser.py
+++ SystemPrograms/Browser/Browser.py
@@ -1,28 +1,88 @@
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtWebEngineWidgets import *
+from PyQt5.QtPrintSupport import *
+import sys
+ 
+class MainWindow(QMainWindow):
+ 
+    # Browser Maker
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+ 
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl("http://google.com"))
+        self.browser.urlChanged.connect(self.update_urlbar)
+        self.browser.loadFinished.connect(self.update_title)
+        self.setCentralWidget(self.browser)
+
+        self.status = QStatusBar()
+        self.setStatusBar(self.status)
+ 
+        navtb = QToolBar("Navigation")
+        self.addToolBar(navtb)
+ 
+        back_btn = QAction("<", self)
+        back_btn.setStatusTip("Back to previous page")
+        back_btn.triggered.connect(self.browser.back)
+        navtb.addAction(back_btn)
+
+        next_btn = QAction(">", self)
+        next_btn.setStatusTip("Forward to next page")
+        next_btn.triggered.connect(self.browser.forward)
+        navtb.addAction(next_btn)
+
+        reload_btn = QAction("Reload", self)
+        reload_btn.setStatusTip("Reload page")
+        reload_btn.triggered.connect(self.browser.reload)
+        navtb.addAction(reload_btn)
+
+        home_btn = QAction("Home", self)
+        home_btn.setStatusTip("Go home")
+        home_btn.triggered.connect(self.navigate_home)
+        navtb.addAction(home_btn)
+ 
+        navtb.addSeparator()
+
+        self.urlbar = QLineEdit()
+        self.urlbar.returnPressed.connect(self.navigate_to_url)
+        navtb.addWidget(self.urlbar)
+
+        stop_btn = QAction("X", self)
+        stop_btn.setStatusTip("Stop loading current page")
+        stop_btn.triggered.connect(self.browser.stop)
+        navtb.addAction(stop_btn)
+ 
+        self.show()
+ 
+ 
+    # title updater
+    def update_title(self):
+        title = self.browser.page().title()
+        self.setWindowTitle("% s - PyOS Browser" % title)
+ 
+ 
+    # home buttton
+    def navigate_home(self):
+        self.browser.setUrl(QUrl("http://www.google.com"))
+ 
+ 
+    def navigate_to_url(self):
+        q = QUrl(self.urlbar.text())
+        if q.scheme() == "":
+            q.setScheme("http")
+        self.browser.setUrl(q)
+ 
+    def update_urlbar(self, q):
+        self.urlbar.setText(q.toString())
+        self.urlbar.setCursorPosition(0)
+ 
 def Main(OS):
+ 
+    app = QApplication(sys.argv)
+    app.setApplicationName("PyOS Browser")
+    window = MainWindow()
 
-    print("DOES NOT WORK PROPERLY!")
-
-    # import sys
-    # from PyQt5.QtCore import QUrl
-    # from PyQt5.QtWidgets import QApplication, QVBoxLayout, QWidget
-    # from PyQt5.QtWebEngineWidgets import QWebEngineView
-
-    # class Browser(QWidget):
-    #     def __init__(self):
-    #         super().__init__()
-    #         self.setWindowTitle('Py Chart')
-    #         self.setGeometry(100, 100, 800, 600)
-    #         self.create_browser()
-
-    #     def create_browser(self):
-    #         self.browser = QWebEngineView()
-    #         self.browser.setUrl(QUrl('https://www.google.com'))
-    #         layout = QVBoxLayout()
-    #         layout.addWidget(self.browser)
-    #         self.setLayout(layout)
-
-    # if True:
-    #     app = QApplication(sys.argv)
-    #     browser = Browser()
-    #     browser.show()
-    #     browser.exit(app.exec_())
+    app.exec_()
+    sys,exit(app.exec_)
