27aaf5421911359b88bef64beb4cb2e278412960
kraken5567 <paddymurph2005@gmail.com>
2023-12-26 17:38:44 -0500

    added better update logic

--- BootLoader.py
+++ BootLoader.py
@@ -9,8 +9,25 @@ def Boot(OS):
     app_reg = initApps(OS, screen, core_iconinfo[0], core_iconinfo[1])
     reloader_reg = initReloader(OS, screen, programbar, core_iconinfo, sys_reg, app_reg)
 
+    wallpaper.load_gif()
+
+    worked = True
+
     while OS != None:
-        UpdateOSDisplay(OS, wallpaper)
+        if (worked):
+            try:
+                OS.update()
+                screen.update()
+                wallpaper.update()
+            except AttributeError:
+                worked = False
+
+                OS.update()
+                screen.update()
+        else:
+            OS.update()
+            screen = OS.winfo_children()[0]
+            screen.update()
 
     #fix gif
     # - Goes away on reload
